336b8cf16568f290839665886f4b1f58c5bf2685
Pratham Mittal <87816515+PrathamMittal17@users.noreply.github.com>
2022-07-17 15:02:23 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 model = pickle.load(open("model.pkl", "rb"))
